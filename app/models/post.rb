f27c3e983cf57b9a2d5788fcf9bde80bb958f195
Habip Yesilyurt <habip.yesilyurt@nimbo.com.tr>
2019-10-29 02:05:24 +0300

    Add comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  #the post is destroyed the  comments that are dependent on it are also destroyed
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: {minimum: 5}
   validates :body, presence: true
 end
